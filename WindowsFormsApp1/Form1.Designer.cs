67d21633ba857ea6604a2bd31e2cbf83ba98b54c
Eqinoz <extazybaddy79@outlook.com>
2024-03-03 13:44:30 +0300

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(787, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 67);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(109, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 95);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 29);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Gönder";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 6);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 36);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Başla";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(127, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(296, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Millimeter, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(252, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 55);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Millimeter, ((byte)(162)));
+            this.label5.ForeColor = System.Drawing.Color.Lime;
+            this.label5.Location = new System.Drawing.Point(327, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 55);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "0";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(109, 97);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(91, 29);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Geç";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(108, 6);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(90, 36);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Pas";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(843, 533);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Word Top 1K";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient; // SQL Server için gerekli kütüphane
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        SqlCommand komut;
+        SqlConnection baglan;
+        SqlDataAdapter da;
+        DataSet ds;
+
+
+
+        void listele()
+        {
+           baglan = new SqlConnection("server=SINAN\\YOSHI; Initial Catalog=word_1k;Integrated Security=SSPI");
+            da = new SqlDataAdapter("Select *From word_a", baglan);// veri tabanındaki verileri çekmek için
+            ds = new DataSet();// veri tabanındaki verileri tutmak için
+            baglan.Open();
+            da.Fill(ds, "word_a");
+            baglan.Close();
+        }
+        public int sayi, kod;// sayi değişkeni rastgele sayı üretmek için, kod değişkeni ascii kodunu tutmak için
+        public string kelime;// kelime değişkeni kelimeyi tutmak için
+        public char harf;// harf değişkeni tablo adını tutmak için
+        public Form1()
+        {
+            InitializeComponent();
+            
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+            listele();
+            label1.Hide();
+            label3.Hide();
+            label4.Text=0.ToString();
+            label5.Text = 0.ToString();
+            
+            
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();//Random sınıfı
+            kod = rnd.Next(97, 119);// 97-119 arası rastgele sayı üretir bunlar ascii kodlarıdır
+            harf = Convert.ToChar(kod);// ascii kodunu harfe çevirir
+
+
+            komut = new SqlCommand("Select Count(id) from word_"+harf+"", baglan);// harf değişkeni ile tablo adını birleştirerek tablodaki kelime sayısını bulur
+            SqlDataReader dr;// veri tabanından veri okumak için
+            baglan.Open();
+            dr = komut.ExecuteReader();// komutu çalıştırır
+            while (dr.Read())// veri tabanından veri okur
+            {
+                label1.Text = dr.GetValue(0).ToString();// tablodaki kelime sayısını label1 e yazdırır
+
+            }
+
+            baglan.Close();
+            
+            sayi = rnd.Next(1, Convert.ToInt16(label1.Text));// 1 ile tablodaki kelime sayısı arasında rastgele sayı üretir
+
+           
+            komut = new SqlCommand("Select word, meaning from word_"+harf+" where id="+sayi+"", baglan);// harf değişkeni ile tablo adını birleştirerek tablodaki kelime sayısını bulur
+            SqlDataReader dr1;
+            baglan.Open();
+            dr1= komut.ExecuteReader();
+            while(dr1.Read())
+            {
+                label2.Text = dr1.GetValue(0).ToString();//tablodaki word sütunundaki veriyi label2 ye yazdırır
+                kelime = dr1.GetValue(1).ToString();//tablodaki meaning sütunundaki veriyi kelime değişkenine atar
+                
+            }
+            
+            baglan.Close();
+            
+            
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            button1_Click(sender, e);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text="";
+            button2_Click(sender, e);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = kelime;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            if (textBox1.Text==kelime)
+            {
+                label3.Show();
+                label3.Text = "Doğru Cevap";
+                label3.ForeColor = Color.Green;
+                label5.Text = (Convert.ToInt32(label5.Text) + 1).ToString();
+                textBox1.Text = "";
+                //MessageBox.Show("Doğru Cevap");
+            }
+            else
+            {
+               label4.Text = (Convert.ToInt32(label4.Text) + 1).ToString();
+                label3.Show();
+                label3.Text = kelime;
+                label3.ForeColor = Color.Red;
+                //MessageBox.Show("Yanlış Cevap");
+                textBox1.Text = "";
+            }
+           button2_Click(sender, e);
+
+
+        }
+    }
+}
